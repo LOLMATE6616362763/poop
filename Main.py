8af63025f248f2d037975dc0f3f2b264ec73f604
LOLMATE6616362763 <86683065+LOLMATE6616362763@users.noreply.github.com>
2021-07-07 17:54:50 +0300

    Main.py

---
+++ Main.py
@@ -0,0 +1,43 @@
+import cs
+from colorama import Fore
+
+
+os.system("cls && title AsmoMultiTool")
+
+
+def Design():
+  os.system("cls && title AsmoMultiTool")
+  print(f```
+        
+{Fore.PURPLE} ▄▄▄        ██████  ███▄ ▄███▓ ▒█████  
+{Fore.PURPLE}▒████▄    ▒██    ▒ ▓██▒▀█▀ ██▒▒██▒  ██▒
+{Fore.PURPLE}▒██  ▀█▄  ░ ▓██▄   ▓██    ▓██░▒██░  ██▒
+{Fore.PURPLE}░██▄▄▄▄██   ▒   ██▒▒██    ▒██ ▒██   ██░
+{Fore.PURPLE}       ▓█   ▓██▒▒██████▒▒▒██▒   ░██▒░ ████▓▒░
+{Fore.PURPLE} ▒▒   ▓▒█░▒ ▒▓▒ ▒ ░░ ▒░   ░  ░░ ▒░▒░▒░ 
+{Fore.PURPLE} ▒   ▒▒ ░░ ░▒  ░ ░░  ░      ░  ░ ▒ ▒░ 
+{Fore.PURPLE} ░   ▒   ░  ░  ░  ░      ░   ░ ░ ░ ▒  
+{Fore.PURPLE}░  ░      ░         ░       ░ ░   
+
+                                 {Fore.LIGHTGREEN_EX}Asmo Multitool                  
+                                 {Fore.LIGHTGREEN_EX}By asmo#2021
+        
+ ```)
+print("")  
+print(f`{Fore_LIGHTWHITE_EX}[{Fore_LIGHTCYAN_EX}] {Fore.PURPLE}Youtube{Fore.PINK}Channel')
+print("")
+print("")
+
+answer = input("> ")
+      
+if answer == "1": 
+      webbrowser.open_new_tab('https://www.youtube.com/channel/UCos0XbDFYjKwiBN6K0A9RVw')
+
+else:
+      print("")
+      Design():                
+      
+ResetTool() 
+
+os.system("pause")
+    
