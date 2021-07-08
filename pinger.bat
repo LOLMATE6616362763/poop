db689ec051d41aaefdf5ba27f282aa031e0391c6
LOLMATE6616362763 <86683065+LOLMATE6616362763@users.noreply.github.com>
2021-07-08 16:28:38 +0300

    Update pinger.bat

--- pinger.bat
+++ pinger.bat
@@ -1,67 +1,28 @@
+@echo off
+chcp 65001
+cls
+color 5
+title ASMOPINGER
+echo  ▄▄▄        ██████  ███▄ ▄███▓ ▒█████  
+echo ▒████▄    ▒██    ▒ ▓██▒▀█▀ ██▒▒██▒  ██▒
+echo ▒██  ▀█▄  ░ ▓██▄   ▓██    ▓██░▒██░  ██▒
+echo ░██▄▄▄▄██   ▒   ██▒▒██    ▒██ ▒██   ██░
+echo ▓█   ▓██▒▒██████▒▒▒██▒   ░██▒░ ████▓▒░
+echo ▒▒   ▓▒█░▒ ▒▓▒ ▒ ░░ ▒░   ░  ░░ ▒░▒░▒░ 
+echo ▒   ▒▒ ░░ ░▒  ░ ░░  ░      ░  ░ ▒ ▒░ 
+echo ░   ▒   ░  ░  ░  ░      ░   ░ ░ ░ ▒  
+echo   ░  ░      ░         ░       ░ ░  
+echo.
+echo ---------------------------------------
+echo -      CTRL-C TO STOP PINGING         -
+echo ---------------------------------------
 set /p IP-Enter IP:
-:rainbow
+color 3
+:top
 PING -n 1 %IP% | FIND "TTL="
-IF ERRORLEVEL 1 (SET in=c & echo offline...)
+title ::SEARCHING FOR ASMO BIG DICK %IP%
+IF ERRORLEVEL 1 (echo [BIG DICK POUNDED] %IP% [BIG DICK POUNDED])
+set /a num= (%Random%%%9)+1
+color %num%
 ping -t 2 0 10 127.0.0.1 >nul
-
-color a
-PING -n 1 %IP% | FIND "TTL="
-IF ERRORLEVEL 1 (SET in=c & echo offline...)
-ping -t 2 0 10 127.0.0.1 >nul
-color 02
-PING -n 1 %IP% | FIND "TTL="
-IF ERRORLEVEL 1 (SET in=c & echo offline...) 
-ping -t 2 0 10 127.0.0.1 >nul
-color 03
-PING -n 1 %IP% | FIND "TTL="
-IF ERRORLEVEL 1 (SET in=c & echo offline...) 
-ping -t 2 0 10 127.0.0.1 >nul
-color 04
-PING -n 1 %IP% | FIND "TTL="
-IF ERRORLEVEL 1 (SET in=c & echo offline...)
-ping -t 2 0 10 127.0.0.1 >nul
-color 05
-PING -n 1 %IP% | FIND "TTL="
-IF ERRORLEVEL 1 (SET in=c & echo offline...) 
-ping -t 2 0 10 127.0.0.1 >nul
-color 06
-PING -n 1 %IP% | FIND "TTL="
-IF ERRORLEVEL 1 (SET in=c & echo offline...) 
-ping -t 2 0 10 127.0.0.1 >nul
-color 07
-PING -n 1 %IP% | FIND "TTL="
-IF ERRORLEVEL 1 (SET in=c & echo offline...) 
-ping -t 2 0 10 127.0.0.1 >nul
-color 08
-PING -n 1 %IP% | FIND "TTL="
-IF ERRORLEVEL 1 (SET in=c & echo offline...) 
-ping -t 2 0 10 127.0.0.1 >nul
-color 09
-PING -n 1 %IP% | FIND "TTL="
-IF ERRORLEVEL 1 (SET in=c & echo offline...)
-ping -t 2 0 10 127.0.0.1 >nul
-color 0a
-PING -n 1 %IP% | FIND "TTL="
-IF ERRORLEVEL 1 (SET in=c & echo offline...)
-ping -t 2 0 10 127.0.0.1 >nul
-color 0b
-PING -n 1 %IP% | FIND "TTL="
-IF ERRORLEVEL 1 (SET in=c & echo offline...) 
-ping -t 2 0 10 127.0.0.1 >nul
-color 0c
-PING -n 1 %IP% | FIND "TTL="
-IF ERRORLEVEL 1 (SET in=c & echo offline...) 
-ping -t 2 0 10 127.0.0.1 >nul
-color 0d
-PING -n 1 %IP% | FIND "TTL="
-IF ERRORLEVEL 1 (SET in=c & echo offline...)
-ping -t 2 0 10 127.0.0.1 >nul
-color 0e
-PING -n 1 %IP% | FIND "TTL="
-IF ERRORLEVEL 1 (SET in=c & echo offline...)
-ping -t 2 0 10 127.0.0.1 >nul
-color 0f
-PING -n 1 %IP% | FIND "TTL="
-IF ERRORLEVEL 1 (SET in=c & echo offline...)
-ping -t 2 0 10 127.0.0.1 >nul
-goto rainbow
+GoTo top
