31b514fd6c750bcf7889888eb40425dee9065733
LOLMATE6616362763 <86683065+LOLMATE6616362763@users.noreply.github.com>
2021-07-08 15:55:00 +0300

    Create pinger.bat

---
+++ pinger.bat
@@ -0,0 +1,32 @@
+@echo off & cls
+color 5
+title coded by asmo#2021
+echo  ▄▄▄        ██████  ███▄ ▄███▓ ▒█████  
+echo ▒████▄    ▒██    ▒ ▓██▒▀█▀ ██▒▒██▒  ██▒
+echo ▒██  ▀█▄  ░ ▓██▄   ▓██    ▓██░▒██░  ██▒
+echo ░██▄▄▄▄██   ▒   ██▒▒██    ▒██ ▒██   ██░
+echo ▓█   ▓██▒▒██████▒▒▒██▒   ░██▒░ ████▓▒░
+echo ▒▒   ▓▒█░▒ ▒▓▒ ▒ ░░ ▒░   ░  ░░ ▒░▒░▒░ 
+echo ▒   ▒▒ ░░ ░▒  ░ ░░  ░      ░  ░ ▒ ▒░ 
+echo ░   ▒   ░  ░  ░  ░      ░   ░ ░ ░ ▒  
+echo    ░  ░      ░         ░       ░ ░  
+echo insta- ex
+echo dis- ex
+
+
+set /p IP-Enter IP:
+:top
+color 1
+PING -n 1 %IP% | FIND "TIL-"
+IF ERRORLEVEL 1 (SET OUT-C & echo IP OFFLINE...) ELSE (SET OUT-2)
+color 2
+PING -n 1 %IP% | FIND "TIL-"
+IF ERRORLEVEL 1 (SET OUT-C & echo IP OFFLINE...) ELSE (SET OUT-4)
+color 3
+PING -n 1 %IP% | FIND "TIL-"
+IF ERRORLEVEL 1 (SET OUT-C & echo IP OFFLINE...) ELSE (SET OUT-6)
+GoTo top
+
+
+
+
